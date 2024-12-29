db67b31d7e84aa9e9f0225e0cd6a45b2066c6c2d
Ali hrhera <alihrhera1@gmail.com>
2024-12-29 10:22:43 +0200

    create Registration SDK as separated module

---
+++ registrationsdk/src/main/java/ali/hrhera/registration_sdk/MainActivity.kt
@@ -0,0 +1,32 @@
+package ali.hrhera.registration_sdk
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ali.hrhera.registration_sdk.ui.theme.RegistrationSDKTaskTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RegistrationSDKTaskTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                }
+            }
+        }
+    }
+}
+
+
+
