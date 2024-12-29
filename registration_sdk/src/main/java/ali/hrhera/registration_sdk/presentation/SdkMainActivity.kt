31555c6da8bcaddd5527138b388d71e39f0c0ed9
Ali hrhera <alihrhera1@gmail.com>
2024-12-29 11:41:19 +0200

    start the sdk from the app

--- registration_sdk/src/main/java/ali/hrhera/registration_sdk/presentation/SdkMainActivity.kt
+++ registration_sdk/src/main/java/ali/hrhera/registration_sdk/presentation/SdkMainActivity.kt
@@ -1,5 +1,6 @@
 package ali.hrhera.registration_sdk.presentation
 
+import ali.hrhera.registration_sdk.presentation.routs.AppRouts
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,16 +9,19 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import ali.hrhera.registration_sdk.presentation.ui.theme.RegistrationSDKTaskTheme
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.lifecycle.HiltViewModel
 
 
-class MainActivity : ComponentActivity() {
+@AndroidEntryPoint
+class SdkMainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            RegistrationSDKTaskTheme {
+            RegistrationSDKTaskTheme(darkTheme = false) {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
+                    AppRouts(paddingValues = innerPadding)
                 }
             }
         }
